1dc963bddb2aa030b6599bf0f74a9e6854509aaf
ouissem <57602676+DrOuissem@users.noreply.github.com>
2022-10-26 18:13:22 -0500

    third exercise finalized
    I need to make tests to ensure the results

--- app/src/main/java/com/example/mindrises/IQ/FindWordActivity.kt
+++ app/src/main/java/com/example/mindrises/IQ/FindWordActivity.kt
@@ -1,43 +1,162 @@
 package com.example.mindrises.IQ
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.view.forEach
+import androidx.core.view.get
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mindrises.R
+import kotlinx.android.synthetic.main.activity_find_next.*
 import kotlinx.android.synthetic.main.activity_find_word.*
+import kotlinx.android.synthetic.main.activity_find_word.nb_correct
+import kotlinx.android.synthetic.main.activity_find_word.nb_wrong
 
 class FindWordActivity : AppCompatActivity() {
 
     var findWordModel: FindWordModel?=null
-    var array:ArrayList<Button>?=null
+    var listButtons:ArrayList<Button>?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_find_word)
-        array=arrayListOf<Button>(letter1,letter2,letter3,letter4,letter5,letter6,letter7,letter8)
+        listButtons=arrayListOf<Button>(letter1,letter2,letter3,letter4,letter5,letter6,letter7,letter8)
+        var b = gridLayout.get(0) as Button
+        b.text="Z"
+
         findWordModel= ViewModelProvider(this).get(FindWordModel::class.java)
         var word_selected : MutableLiveData<String> = findWordModel!!.selectLetter("")
-        word_selected.observe(this, Observer {
+        initGridBlanck()
+        selectButtons()
+        show_letters()
+        findWordModel!!.currentWord.observe(this, Observer {
+            initGridBlanck()
+            selectButtons()
+            show_letters()
+
+        })
+        findWordModel!!.nbCorrect.observe(this,Observer{
+            nb_correct.text=it.toString()
+        })
+        findWordModel!!.nbWrong.observe(this,Observer{
+            nb_wrong.text=it.toString()
+        })
+
+        findWordModel!!.wordSelected.observe(this, Observer {
             selected_word.text=it.toString()
         })
         val intent=getIntent()
+
+    }
+
+    //the buttons to be shown in the screen
+    fun selectButtons(){
+        val randomList = (0..15).shuffled().take(findWordModel!!.currentWord.value!!.length)
+        listButtons?.clear()
+        randomList.forEach {
+            listButtons?.add(gridLayout.get(it) as Button)
+        }
+    }
+
+    //add letter to the buttons and make them visible with black color
+    fun show_letters(){
+        for(i in 0 until listButtons!!.size){
+            listButtons!![i].text = ""+findWordModel!!.currentWord.value!!.get(i)
+            setTextVisible(listButtons!![i])
+
+        }
+    }
+    fun setTextInvisible(b:Button){
+        b.setTextColor(Color.WHITE)
+        b.isClickable=false
+    }
+    fun setAllTextVisible(){
+        listButtons!!.forEach {
+            setTextVisible(it)
+        }
+    }
+    fun setTextVisible(b:Button){
+        b.setTextColor(Color.BLACK)
+        b.isClickable=true
+    }
+    fun initGridBlanck(){
+        gridLayout.forEach {
+            val b = it as Button
+            b.setBackgroundColor(Color.WHITE)
+            b.stateListAnimator=null
+            setTextInvisible(b)
+        }
     }
     fun selectChar(view:View){
         val v=view as Button
-        v.isClickable=false
-
+        setTextInvisible(v)
         this.findWordModel?.selectLetter(v.text.toString()) ?: Toast.makeText(this, "Error selected letter", Toast.LENGTH_SHORT).show()
+        //Log.i("l1 : ${findWordModel!!.cu}")
+        if(findWordModel!!.endSelection()){
+            if (this.findWordModel!!.correctGuess()){
+                answerCorrect()
+                if(this.findWordModel!!.endGame())
+                    endGame()
+            }else {
+                answerWrong()
+            }
+        }
+    }
 
+    fun answerWrong(){
+        findWordModel!!.upCorrect()
+        selected_word.setTextColor(Color.RED)
+        Handler(Looper.getMainLooper()).postDelayed({
+            setAllTextVisible()
+            this.findWordModel?.reinit_selected_word() ?: Toast.makeText(this, "Error retry", Toast.LENGTH_SHORT).show()
+            selected_word.setTextColor(Color.GRAY)
+        }, 1000)
 
     }
 
-    fun retry(view: View) {
-        array?.forEach{it.isClickable=true}
-        this.findWordModel?.reinit_selected_word() ?: Toast.makeText(this, "Error retry", Toast.LENGTH_SHORT).show()
+    fun answerCorrect(){
+        findWordModel!!.upCorrect()
+        selected_word.setTextColor(Color.GREEN)
+        Handler(Looper.getMainLooper()).postDelayed({
+            this.findWordModel!!.reinit_selected_word()
+            selected_word.setTextColor(Color.GRAY)
+            this.findWordModel!!.nextWord()
 
+        }, 1000)
+
+    }
+    fun endGame(){
+        val builder = AlertDialog.Builder(this)
+        refresh.isClickable=false
+        builder.setMessage("You solved ${findWordModel!!.nbCorrect.value} questions out of 10\n" +
+                "do you want to try again")
+            .setPositiveButton("Yes",
+                DialogInterface.OnClickListener { dialog, id ->
+                    // START THE GAME!
+                    findWordModel!!.reInit()
+
+                })
+            .setNegativeButton("No",
+                DialogInterface.OnClickListener { dialog, id ->
+                    // User cancelled the dialog
+                })
+        // Create the AlertDialog object and return it
+        builder.create()
+        builder.show()
+        Toast.makeText(this,"end game",Toast.LENGTH_LONG)
+    }
+    fun retry(view: View) {
+        setAllTextVisible()
+        this.findWordModel?.reinit_selected_word() ?: Toast.makeText(this, "Error retry", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/mindrises/IQ/FindWordModel.kt
+++ app/src/main/java/com/example/mindrises/IQ/FindWordModel.kt
@@ -17,32 +17,36 @@ import java.util.*
 
 
 class FindWordModel(application:Application):AndroidViewModel(application) {
-    private val two_letters= arrayOf(R.array.two_letters)
-    private val three_letters= arrayOf(R.array.three_letters)
-    private val four_letters= arrayOf(R.array.four_letters)
-    private val five_letters= arrayOf(R.array.five_letters)
-    private val other_letters= arrayOf(R.array.others)
-
-    private val readAllData: LiveData<List<Word>>
-    private val repository: WordRepository
-    private var wordToGuess: String ="ABSOLUTE";
-    var wordShuffled= MutableLiveData<String>()
-    private var length=8;
-    private var nb_selected=0;
+    private val two_letters= application.getResources().getStringArray(R.array.two_letters)
+    private val three_letters= application.getResources().getStringArray(R.array.three_letters)
+    private val four_letters= application.getResources().getStringArray(R.array.four_letters)
+    private val five_letters= application.getResources().getStringArray(R.array.five_letters)
+    private val other_letters= application.getResources().getStringArray(R.array.others)
+    var nbCorrect=MutableLiveData<Int>()
+    var nbWrong=MutableLiveData<Int>()
+    //private val readAllData: LiveData<List<Word>>
+    //private val repository: WordRepository
+    //private var wordToGuess: String ="ABSOLUTE";
+    //var wordShuffled= MutableLiveData<String>()
+    //private var length=8;
+    //private var nb_selected=0;
     var currentWord= MutableLiveData<String>()
     private var index=0
-    private var wordSelected=MutableLiveData<String>()
-     var words = ArrayList<Word>()
+    var wordSelected=MutableLiveData<String>()
+    var words = ArrayList<Word>()
     fun shuffle(s: String): String {
         val helloCharArray = s.toCharArray()
         helloCharArray.shuffle()
         return helloCharArray.concatToString()
     }
+    fun endSelection()=wordSelected.value!!.length==currentWord.value!!.length
+    fun endGame()=index==words.size
+    fun correctGuess()=currentWord.value.equals(wordSelected.value)
     fun createListWords(){
         words.clear()
         words.add(Word(0,two_letters[0].toString(),two_letters[0].toString().length,"en"))
         words.add(Word(0,three_letters[0].toString(),three_letters[0].toString().length,"en"))
-        words.add(Word(0,three_letters[1].toString(),three_letters[1].toString().length,"en"))
+        words.add(Word(0,three_letters.get(1).toString(),three_letters.get(1).toString().length,"en"))
         words.add(Word(0,three_letters[2].toString(),three_letters[2].toString().length,"en"))
         words.add(Word(0,four_letters[0].toString(),four_letters[0].toString().length,"en"))
         words.add(Word(0,four_letters[1].toString(),four_letters[1].toString().length,"en"))
@@ -52,32 +56,43 @@ class FindWordModel(application:Application):AndroidViewModel(application) {
         words.add(Word(0,other_letters[1].toString(),other_letters[1].toString().length,"en"))
 
     }
+    fun upCorrect(){nbCorrect.value=nbCorrect.value!!+1}
+    fun upWrong(){nbWrong.value=nbWrong.value!!+1}
     fun nextWord(){
-        wordToGuess=words[index].word
-        wordShuffled.value=shuffle(wordToGuess)
+        currentWord.value=words[index].word
+
         if(index<words.size){
             index++;
         }
-        wordSelected.value=words[index].word
+        wordSelected.value=""
+
+    }
+    fun reInit(){
+        nbCorrect.value=0
+        nbWrong.value=0
+        index=0
+        createListWords()
+        nextWord()
+        wordSelected.value=""
     }
     init {
-        //createListWords()
-        //currentWord=words[index].word
-
+        nbCorrect.value=0
+        nbWrong.value=0
+        createListWords()
+        nextWord()
         wordSelected.value=""
-        val wordDao = WordDatabase.getDatabase(application).getWordDao()
-        repository = WordRepository(wordDao)
-        readAllData=repository.readAllData
+        //val wordDao = WordDatabase.getDatabase(application).getWordDao()
+        //repository = WordRepository(wordDao)
+        //readAllData=repository.readAllData
 
         //Log.i("size is : ",readAllData.value!!.size.toString())
-        if(readAllData.value==null || readAllData.value!!.size==0)addWords()
-        if(repository==null)
-        Log.i("size is : ","repo null")
-        else Log.i("size is : ","repo not null")
+        //if(readAllData.value==null || readAllData.value!!.size==0)addWords()
+        //if(repository==null)
+
     }
     fun addWord(word:Word){
         viewModelScope.launch (Dispatchers.IO){
-            repository.addWord(word)
+            //repository.addWord(word)
         }
     }
     fun addWords(){
